bc7a724b1c2adddf884e6bbe08c9f78a19a5e827
Toni Kemppainen <tonikem@gmail.com>
2024-12-14 19:07:17 +0200

    Tunti 1

---
+++ Tunti 1/1. sovellusten asentaminen.py
@@ -0,0 +1,7 @@
+## Lataa ohjelmat alla olevista linkeistä:
+
+# https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe
+# https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=windows&code=PCC
+# https://github.com/git-for-windows/git/releases/download/v2.47.1.windows.1/Git-2.47.1-64-bit.exe
+
+
