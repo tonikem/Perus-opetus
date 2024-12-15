f8bbea9262e2ee98a18d8eeffccc1053a84d7c88
Toni Kemppainen <tonikem@gmail.com>
2024-12-15 09:05:30 +0200

    Tunti 2

--- Tunti 2/6. lisää funktioista (parametrit ym.).py
+++ Tunti 2/6. lisää funktioista (parametrit ym.).py
@@ -1,18 +1,28 @@
 
-
+## 1.
 def summa(x, y):
     print("Parametrien summa on", x + y)
-
 
 summa(1, 2)
 summa(y=2, x=1)
 
 
+## 2.
 def paluuarvo():
     return "Tämä merkkijono palautuu funktiosta."
 
-
 muuttuja = paluuarvo()
 print(muuttuja)
+
+
+## 3.
+def summa(a, b):
+    return a+b
+
+def erotus(a, b):
+    return a-b
+
+tulos = erotus(summa(5, 2), summa(2, 3))
+print("Vastaus on", tulos)
 
 
